928303f9aa46e08d8e5d0ba281f4981912b65837
Brayden Chan <braydenmchan@gmail.com>
2023-03-17 16:25:40 -0700

    Add module-info file.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.megabyte6.pixellauncher {
+    requires static lombok;
+
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+
+    exports com.megabyte6.pixellauncher;
+}
