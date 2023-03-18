b20de51467cf1292988a2a7d183a73b1f1856cd0
Brayden Chan <braydenmchan@gmail.com>
2023-03-18 11:39:53 -0700

    Fix stage not showing.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module org.pixellauncher {
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
 
+    opens org.pixellauncher.controller to javafx.fxml;
+
     exports org.pixellauncher;
 }
--- src/main/java/org/pixellauncher/App.java
+++ src/main/java/org/pixellauncher/App.java
@@ -2,6 +2,7 @@ package org.pixellauncher;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -12,7 +13,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        final Scene scene = ResourceLoader.loadFXML("fxml/Main.fxml").load();
+        final Pane pane = ResourceLoader.loadFXML("fxml/Main.fxml").load();
+        final Scene scene = new Scene(pane);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Pixel Launcher");
