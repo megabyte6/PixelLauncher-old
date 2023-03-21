9dca42270b696d42a8b904300e02b83c5835b704
Brayden Chan <braydenmchan@gmail.com>
2023-03-21 12:03:01 -0700

    Add temp icon.

--- src/main/java/org/pixellauncher/App.java
+++ src/main/java/org/pixellauncher/App.java
@@ -1,8 +1,11 @@
 package org.pixellauncher;
 
+import io.github.palexdev.materialfx.css.themes.MFXThemeManager;
+import io.github.palexdev.materialfx.css.themes.Themes;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -13,10 +16,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        final Pane pane = ResourceLoader.loadFXML("fxml/Main.fxml").load();
-        final Scene scene = new Scene(pane);
+        final Parent root = ResourceLoader.loadFXML("fxml/Main.fxml").load();
+        final Scene scene = new Scene(root);
+        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
 
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(ResourceLoader.loadStream("logo.png")));
         primaryStage.setTitle("Pixel Launcher");
         primaryStage.show();
     }
