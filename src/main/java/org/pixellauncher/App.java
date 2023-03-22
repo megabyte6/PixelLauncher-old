b18d935896b7e1ad98da768cd52fa50abe759e3c
Brayden Chan <braydenmchan@gmail.com>
2023-03-22 13:29:35 -0700

    Show theme on startup.

--- src/main/java/org/pixellauncher/App.java
+++ src/main/java/org/pixellauncher/App.java
@@ -7,10 +7,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
+import org.pixellauncher.constant.Constants;
+import org.pixellauncher.setting.Settings;
+import org.pixellauncher.setting.Theme;
 
 public class App extends Application {
 
+    @Getter
+    @Setter
+    private static Settings settings;
+
     public static void main(String[] args) {
+        settings = Settings.loadElseDefault(Constants.CONFIG_PATH);
+
         launch(args);
     }
 
@@ -18,11 +29,14 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         final Parent root = ResourceLoader.loadFXML("fxml/Main.fxml").load();
         final Scene scene = new Scene(root);
+
+        final String styleSheetPath = Theme.getStyleSheet(getSettings().getTheme()).toString();
+        scene.getStylesheets().setAll(styleSheetPath);
         MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
 
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image(ResourceLoader.loadStream("logo.png")));
-        primaryStage.setTitle("Pixel Launcher");
+        primaryStage.setTitle(Constants.LAUNCHER_NAME_FORMATTED);
         primaryStage.show();
     }
 
