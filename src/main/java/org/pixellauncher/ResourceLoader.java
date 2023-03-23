913133b3e5f4973fe0aef37690fc5d952ca60382
Brayden Chan <braydenmchan@gmail.com>
2023-03-22 21:22:37 -0700

    Add docs.

--- src/main/java/org/pixellauncher/ResourceLoader.java
+++ src/main/java/org/pixellauncher/ResourceLoader.java
@@ -1,21 +1,40 @@
 package org.pixellauncher;
 
+import javafx.fxml.FXMLLoader;
+
 import java.io.InputStream;
 import java.net.URL;
-import javafx.fxml.FXMLLoader;
 
 public class ResourceLoader {
 
-    private ResourceLoader() {}
+    private ResourceLoader() {
+    }
 
+    /**
+     * @param path The location of the resource to load. Excluding the
+     *             {@code assets} directory.
+     * @return A {@link URL} representing the resource.
+     */
     public static URL loadURL(String path) {
         return ResourceLoader.class.getResource(path);
     }
 
+    /**
+     * @param path The location of the resource to load. Excluding the
+     *             {@code assets} directory.
+     * @return An {@link InputStream} of the resource.
+     */
     public static InputStream loadStream(String path) {
         return ResourceLoader.class.getResourceAsStream(path);
     }
 
+    /**
+     * Call {@code load()} to load the fxml and call the controller.
+     *
+     * @param path The location of the {@code fxml} file to load.
+     * @return An {@link FXMLLoader} representing the {@code fxml} file
+     * given.
+     */
     public static FXMLLoader loadFXML(String path) {
         return new FXMLLoader(loadURL(path));
     }
