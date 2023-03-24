7a3dad9b29421cf177a5302ef7dc349191a5c3b7
Brayden Chan <braydenmchan@gmail.com>
2023-03-23 19:02:31 -0700

    Use open instead of export.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,20 +2,21 @@ module org.pixellauncher {
     // Modules from plugins.
     requires static lombok;
 
-    requires javafx.graphics;
-    requires transitive javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
     opens org.pixellauncher.controller to javafx.fxml;
 
     // Modules from dependencies.
     requires org.kordamp.ikonli.javafx;
 
     requires transitive com.fasterxml.jackson.databind;
-    exports org.pixellauncher.setting to com.fasterxml.jackson.databind;
-    exports org.pixellauncher.ui to com.fasterxml.jackson.databind;
+    opens org.pixellauncher.setting to com.fasterxml.jackson.databind;
+    opens org.pixellauncher.ui to com.fasterxml.jackson.databind;
 
     requires org.apache.logging.log4j;
 
     requires MaterialFX;
 
+    // Export main package.
     exports org.pixellauncher;
 }
